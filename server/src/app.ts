12627d6d03bfcbcab360da847b1a872562b76656
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2025-05-26 18:37:29 +0300

    patch

--- server/src/app.ts
+++ server/src/app.ts
@@ -22,8 +22,8 @@ app.use(morgan('combined', { stream }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 5000;
+app.listen(PORT, '0.0.0.0', () => {
     logger.info('Server started on port %d', PORT);
 });
 
